51a896a7b616236d9b743a2d9e95069f6c69550f
goran9999 <gutovicgoran@gmail.com>
2024-03-20 09:59:00 +0100

    add try catch

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,23 @@ dotenv.config();
 async function bootstrap() {
   const bot = new NestGram(process.env.NESTGRAM_BOT_KEY!, NestgramModule);
 
-  await bot.start();
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    await bot.start();
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  } catch (error) {
+    console.log(error);
+  }
+  process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception:', error);
+    // Implement your logging mechanism here
+    // Consider gracefully shutting down the server and restarting it
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    // Implement your logging mechanism here
+    // Consider gracefully shutting down the server and restarting it
+  });
 }
 bootstrap();
